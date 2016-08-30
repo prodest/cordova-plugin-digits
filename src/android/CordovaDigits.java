343ae225b8bd754edfe234f98301ff609899e649
vizeke <vizeke@gmail.com>
2016-08-30 15:16:59 -0300

    Add crashlytics functions for android

--- src/android/CordovaDigits.java
+++ src/android/CordovaDigits.java
@@ -44,18 +44,39 @@ public class CordovaDigits extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    Log.i(TAG, "executing action " + action);
+    Log.i(TAG, "executing action " + action + " called with options: " + args);
 
     if ("authenticate".equals(action)) {
       authenticate(callbackContext);
     } else if ("logout".equals(action)) {
       logout(callbackContext);
+    } else if (action.equals("addLog")) {
+			addLog(args, callbackContext);
+		} else if (action.equals("sendCrash")) {
+			sendCrash(args, callbackContext);
+		} else if (action.equals("sendNonFatalCrash")) {
+			sendNonFatalCrash(args, callbackContext);
+		} else if (action.equals("setUserIdentifier")) {
+			setUserIdentifier(args, callbackContext);
+		} else if (action.equals("setUserName")) {
+			setUserName(args, callbackContext);
+		} else if (action.equals("setUserEmail")) {
+			setUserEmail(args, callbackContext);
+		} else if (action.equals("setStringValueForKey")) {
+			setStringValueForKey(args, callbackContext);
+		} else if (action.equals("setIntValueForKey")) {
+			setIntValueForKey(args, callbackContext);
+		} else if (action.equals("setBoolValueForKey")) {
+			setBoolValueForKey(args, callbackContext);
+		} else if (action.equals("setFloatValueForKey")) {
+			setFloatValueForKey(args, callbackContext);
     } else {
       Log.w(TAG, "unknown action `" + action + "`");
-      return false;
-    }
+			callbackContext.error(TAG + ": Method '" + action + "' not supported.");
+			return false;
+		}
 
-    return true;
+		return true;
   }
 
   public void authenticate(final CallbackContext callbackContext) {
@@ -114,4 +135,90 @@ public class CordovaDigits extends CordovaPlugin {
     }
     return null;
   }
+
+
+  /* Crashlytics Events */
+
+	private void addLog(final JSONArray data, final CallbackContext callbackContext) {
+
+		String message = data.optString(0);
+		Crashlytics.log(message);
+		callbackContext.success();
+	}
+
+	private void sendCrash(final JSONArray data, final CallbackContext callbackContext) {
+
+		this.cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				throw new RuntimeException("This is a crash");
+			}
+		});
+	}
+
+	private void sendNonFatalCrash(final JSONArray data, final CallbackContext callbackContext) {
+
+		this.cordova.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Crashlytics.logException(new Throwable(data.optString(0, "No Message Provided")));
+			}
+		});
+	}
+
+	private void setUserIdentifier(final JSONArray data, final CallbackContext callbackContext) {
+
+		String identifier = data.optString(0);
+		Crashlytics.setUserIdentifier(identifier);
+		callbackContext.success();
+	}
+
+	private void setUserName(final JSONArray data, final CallbackContext callbackContext) {
+
+		String userName = data.optString(0);
+		Crashlytics.setUserName(userName);
+		callbackContext.success();
+	}
+
+	private void setUserEmail(final JSONArray data, final CallbackContext callbackContext) {
+
+		String email = data.optString(0);
+		Crashlytics.setUserEmail(email);
+		callbackContext.success();
+	}
+
+	private void setStringValueForKey(final JSONArray data, final CallbackContext callbackContext) {
+
+		String value = data.optString(0);
+		String key = data.optString(1);
+		Crashlytics.setString(key, value);
+		callbackContext.success();
+	}
+
+	private void setIntValueForKey(final JSONArray data, final CallbackContext callbackContext) {
+
+		int value = data.optInt(0);
+		String key = data.optString(1);
+
+		Crashlytics.setInt(key, value);
+		callbackContext.success();
+	}
+
+	private void setBoolValueForKey(final JSONArray data, final CallbackContext callbackContext) {
+
+		boolean value = data.optBoolean(0);
+		String key = data.optString(1);
+
+		Crashlytics.setBool(key, value);
+		callbackContext.success();
+	}
+
+	private void setFloatValueForKey(final JSONArray data, final CallbackContext callbackContext) {
+
+		double value = data.optDouble(0);
+		String key = data.optString(1);
+
+		Crashlytics.setDouble(key, value);
+		callbackContext.success();
+	}
 }
--- www/digits.js
+++ www/digits.js
@@ -1,17 +1,64 @@
-const exec = require('cordova/exec');
+const exec = require( 'cordova/exec' );
 
-function noop() {}
+function noop() { }
 
 const digits = {
-  authenticate: function authenticate(options, authenticateSuccess, authenticateFailed) {
-    exec(function success(result) {
-      (authenticateSuccess || noop)(JSON.parse(result));
-    }, authenticateFailed || noop, 'Digits', 'authenticate', [ options ]);
+  authenticate: function authenticate( options, authenticateSuccess, authenticateFailed ) {
+    exec( function success( result ) {
+      ( authenticateSuccess || noop )( JSON.parse( result ) );
+    }, authenticateFailed || noop, 'Digits', 'authenticate', [ options ] );
   },
 
   logout: function logout() {
-    exec(noop, noop, 'Digits', 'logout', []);
+    exec( noop, noop, 'Digits', 'logout', [] );
   },
+
+  /* Crashalytics */
+
+  addLog: function addLog( message ) {
+    exec( noop, noop, 'Digits', 'addLog', [ message ] );
+  },
+
+  sendCrash: function sendCrash() {
+    exec( noop, noop, 'Digits', 'sendCrash', [] );
+  },
+
+  sendNonFatalCrash: function sendNonFatalCrash( message ) {
+    exec( noop, noop, 'Digits', 'sendNonFatalCrash', [ message ] );
+  },
+
+  recordError: function recordError( message, code ) {
+    exec( noop, noop, 'Digits', 'recordError', [ message, code ] );
+  },
+
+  setUserIdentifier: function setUserIdentifier( userIdentifier ) {
+    exec( noop, noop, 'Digits', 'setUserIdentifier', [ userIdentifier ] );
+  },
+
+  setUserName: function setUserName( userName ) {
+    exec( noop, noop, 'Digits', 'setUserName', [ userName ] );
+  },
+
+  setUserEmail: function setUserEmail( userEmail ) {
+    exec( noop, noop, 'Digits', 'setUserEmail', [ userEmail ] );
+  },
+
+  setStringValueForKey: function setStringValueForKey( value, key ) {
+    exec( noop, noop, 'Digits', 'setStringValueForKey', [ value, key ] );
+  },
+
+  setIntValueForKey: function setIntValueForKey( value, key ) {
+    exec( noop, noop, 'Digits', 'setIntValueForKey', [ value, key ] );
+  },
+
+  setBoolValueForKey: function setBoolValueForKey( value, key ) {
+    exec( noop, noop, 'Digits', 'setBoolValueForKey', [ value, key ] );
+  },
+
+  setFloatValueForKey: function setFloatValueForKey( value, key ) {
+    exec( noop, noop, 'Digits', 'setFloatValueForKey', [ value, key ] );
+  }
+
 };
 
 module.exports = digits;
