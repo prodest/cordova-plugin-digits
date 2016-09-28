6ed70fb73e5f0e4244653037e3dc4881496058c4
vizeke <vizeke@gmail.com>
2016-09-28 13:18:41 -0300

    Add Crashlytics and Answers support for iOS

--- src/ios/CDVDigits.h
+++ src/ios/CDVDigits.h
@@ -2,7 +2,36 @@
 
 @interface CDVDigits : CDVPlugin
 
+//Digits
 - (void)authenticate:(CDVInvokedUrlCommand*)command;
 - (void)logout:(CDVInvokedUrlCommand*)command;
+
+// Crashlytics
+- (void)addLog:(CDVInvokedUrlCommand*)command;
+- (void)setUserIdentifier:(CDVInvokedUrlCommand*)command;
+- (void)setUserName:(CDVInvokedUrlCommand*)command;
+- (void)setUserEmail:(CDVInvokedUrlCommand*)command;
+- (void)sendCrash:(CDVInvokedUrlCommand*)command;
+- (void)setStringValueForKey:(CDVInvokedUrlCommand*)command;
+- (void)setIntValueForKey:(CDVInvokedUrlCommand*)command;
+- (void)setBoolValueForKey:(CDVInvokedUrlCommand*)command;
+- (void)setFloatValueForKey:(CDVInvokedUrlCommand*)command;
+- (void)recordError:(CDVInvokedUrlCommand*)command;
+- (void)sendNonFatalCrash:(CDVInvokedUrlCommand*)command;
+
+// Answers
+- (void)sendPurchase:(CDVInvokedUrlCommand*)command;
+- (void)sendAddToCart:(CDVInvokedUrlCommand*)command;
+- (void)sendStartCheckout:(CDVInvokedUrlCommand*)command;
+- (void)sendSearch:(CDVInvokedUrlCommand*)command;
+- (void)sendShare:(CDVInvokedUrlCommand*)command;
+- (void)sendRatedContent:(CDVInvokedUrlCommand*)command;
+- (void)sendSignUp:(CDVInvokedUrlCommand*)command;
+- (void)sendLogIn:(CDVInvokedUrlCommand*)command;
+- (void)sendInvite:(CDVInvokedUrlCommand*)command;
+- (void)sendLevelStart:(CDVInvokedUrlCommand*)command;
+- (void)sendLevelEnd:(CDVInvokedUrlCommand*)command;
+- (void)sendContentView:(CDVInvokedUrlCommand*)command;
+- (void)sendCustomEvent:(CDVInvokedUrlCommand*)command;
 
 @end
